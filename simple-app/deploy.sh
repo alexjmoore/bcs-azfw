aade36641c9930cc017fc465d87c344f28571f2c
Alex Moore <alex.moore@capside.com>
2018-10-18 00:14:41 +0100

    updated pipeline

--- simple-app/deploy.sh
+++ simple-app/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 apt-get update && apt-get install -y apache2 php libapache2-mod-php
-cp example-app/src/* /var/ww/site
+cp example-app/src/* /var/www/site
 cp example-app/apache-config.conf /etc/apache2/sites-enabled/000-default.conf
