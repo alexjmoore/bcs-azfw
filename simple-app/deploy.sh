38a36fd82c0c654a13dc1b29de4bea9f9a79da62
Alex Moore <alex.moore@capside.com>
2018-10-18 01:20:56 +0100

    updated pipeline

--- simple-app/deploy.sh
+++ simple-app/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 apt-get update && apt-get install -y apache2 php libapache2-mod-php
+mkdir /var/www/site
 cp src/* /var/www/site
 cp apache-config.conf /etc/apache2/sites-enabled/000-default.conf
+systemctl restart apache2
