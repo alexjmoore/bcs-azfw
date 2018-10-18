d447a8bc7e97ffe5cae3003ed604e928d16e3f64
Alex Moore <alex.moore@capside.com>
2018-10-18 22:18:03 +0100

    updated deployment script

--- simple-app/deploy.sh
+++ simple-app/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+echo installing Apache
 apt-get update && apt-get install -y apache2 php libapache2-mod-php
+echo deploying Application
 mkdir /var/www/site
 cp src/* /var/www/site
 cp apache-config.conf /etc/apache2/sites-enabled/000-default.conf
+echo restarting Apache
 systemctl restart apache2
