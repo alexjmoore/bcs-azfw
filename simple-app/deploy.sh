ac6e66e72211dfddfefd80ba9ec6088af7398e37
amoore <alex.moore@capside.com>
2018-10-19 16:13:34 +0100

    updated

--- simple-app/deploy.sh
+++ simple-app/deploy.sh
@@ -7,7 +7,7 @@ mkdir /var/www/site
 cp src/* /var/www/site
 cp apache-config.conf /etc/apache2/sites-enabled/000-default.conf
 echo storing region
-LOCATION=curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-08-01&format=text"
+LOCATION=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-08-01&format=text"`
 echo SetEnv HTTP_LOCATION "$LOCATION" > /var/www/site/.htaccess
 echo restarting Apache
 systemctl restart apache2
